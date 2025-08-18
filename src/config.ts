4ef709b9d78641585740f80d1c04551cd46a9665
ProjectCisco <sga.accounts@proton.me>
2025-08-18 14:06:52 +0100

    refactor(config): realtime and cloud channels added

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,20 @@
-import { CorsOptions } from 'cors'
-import { config as env } from 'dotenv'
-import path from 'path'
+import { CorsOptions } from 'cors';
+import { config as env } from 'dotenv';
+import path from 'path';
 
-export * from './config/constants'
+export * from './config/constants';
 
-const nodeEnv = process.env.NODE_ENV ?? 'development'
+const nodeEnv = process.env.NODE_ENV ?? 'development';
 env({
   path: path.resolve(`.env.${nodeEnv}`),
-})
+});
 
 const cors: CorsOptions = {
   origin: process.env.CORS ?? '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
   exposedHeaders: ['x-auth-token'],
-}
+};
 
 const discord = {
   clientId: process.env.BOT_CLIENT_ID ?? '',
@@ -22,8 +22,12 @@ const discord = {
   guildId: process.env.DISCORD_GUILD_ID ?? '',
   token: process.env.BOT_TOKEN ?? '',
   channels: {
-    gamesReport: process.env.CHANNEL_GAMEREPORTING_ID,
-    botTesting: process.env.CHANNEL_BOT_COMMANDS_ID!,
+    botTesting: 
+      process.env.CHANNEL_BOT_COMMANDS_ID!,
+    realtimeUploads:
+      process.env.CHANNEL_REALTIME_UPLOADS_ID ?? '',
+    cloudUploads:
+      process.env.CHANNEL_CLOUD_UPLOADS_ID ?? '',
   },
   roles: {
     moderator: process.env.ROLE_MODERATOR!,
@@ -31,14 +35,7 @@ const discord = {
     civ6Rank: process.env.ROLE_CIV6!,
     civ7Rank: process.env.ROLE_CIV7!,
   },
-}
-
-const trueskill = {
-  MU: 1250,
-  SIGMA: 150,
-  BETA: 400,
-  TAU: 10,
-}
+};
 
 export const config = {
   oauth: `https://discord.com/api/oauth2/authorize?client_id=${
@@ -49,7 +46,6 @@ export const config = {
   discord,
   host: process.env.HOST!,
   port: Number(process.env.PORT!),
-  trueskill,
   mongoDb: process.env.MONGODB_URL!,
   backend: {
     /** Python-reporting API */
@@ -60,4 +56,4 @@ export const config = {
     windowMs: Number(process.env.RATE_LIMIT_WINDOW_MS) || 15 * 60 * 1000,
     max: Number(process.env.RATE_LIMIT_MAX) || 100,
   },
-}
+};
