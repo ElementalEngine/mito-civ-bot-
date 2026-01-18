51a8e80a3d68e83f77d3762af2b975343dc56e82
Cal Calcifer <calcalciffer@gmail.com>
2026-01-18 01:12:35 +0000

    allow reporter to report tie as well

--- src/commands/reporting/change-report-order.ts
+++ src/commands/reporting/change-report-order.ts
@@ -61,8 +61,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
       await interaction.editReply(`${EMOJI_FAIL} Only original reporter <@${getMatchRes?.reporter_discord_id}> or a moderator can change report order`);
       return;
     }
-    var isModerator = interaction.member.roles.cache.has(config.discord.roles.moderator);
-    if (isValidOrder(newOrder, getMatchRes.players, isModerator) === false) {
+    if (isValidOrder(newOrder, getMatchRes.players) === false) {
       await interaction.editReply(`${EMOJI_FAIL} The new order provided is invalid. Or you do not have permission to set a order with tie positions.`);
       return;
     }
