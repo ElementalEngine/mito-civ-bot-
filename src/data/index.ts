31c5a6b987df7abe5b200192208b39d0309c36e4
ProjectCisco <sga.accounts@proton.me>
2026-01-24 20:47:11 +0000

    refactor: update leader and civilization types and metadata exports

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,25 @@
-export * from "./civ6-leaders";
-export * from "./civ7-civs";
-export * from "./civ7-leaders";
+export type LeaderType =
+  | "Industrial"
+  | "War"
+  | "Naval"
+  | "Culture"
+  | "Religious"
+  | "Science"
+  | "None";
+
+export type AgePool = "Antiquity_Age" | "Exploration_Age" | "Modern_Age";
+
+export type LeaderMeta = Readonly<{
+  gameId: string;
+  emojiId?: string;
+  type: LeaderType;
+}>;
+
+export type CivMeta = Readonly<{
+  gameId: string;
+  emojiId?: string;
+  agePool: AgePool;
+}>;
+
+export * from "./civ6-data";
+export * from "./civ7-data";
